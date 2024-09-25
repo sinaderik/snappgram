1ffc79d56fe0fdcd9787f5dadbc7d358a455dfaf
Sina <sinaderik7@gmail.com>
2024-09-25 14:19:44 +0330

    feat: routes are implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { Route, Routes } from 'react-router-dom'
+import SigninForm from './_auth/forms/SigninForm'
+import SignupFrom from './_auth/forms/SignupFrom'
+import { Home } from './_root/pages'
 import './global.css'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    return (
+        <main className='flex h-screen'>
+            <Routes>
+                {/* public routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SigninForm />} />
+                    <Route path='/sign-up' element={<SignupFrom />} />
+                </Route>
+
+                {/* private routes */}
+                {/* index means this is a starting route */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} /> 
+                </Route>
+            </Routes>
+        </main>
+    )
 }
 
 export default App
