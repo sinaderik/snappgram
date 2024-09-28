233748e01858da4e3b31e1fdd014579992d655c4
Sina <sinaderik7@gmail.com>
2024-09-28 17:39:49 +0330

    feat: import and export approach got changed for the route components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 import SigninForm from './_auth/forms/SigninForm'
 import SignupFrom from './_auth/forms/SignupFrom'
-import { Home } from './_root/pages'
+import {
+    AllUsers,
+    CreatePost,
+    EditPost,
+    Explore,
+    Home,
+    PostDetails,
+    Profile,
+    Saved,
+    UpdateProfile
+} from './_root/pages'
 import './global.css'
 import AuthLayout from './_auth/AuthLayout'
 import RootLayout from './_root/RootLayout'
 import { Toaster } from './components/ui/toaster'
-import Saved from './_root/pages/Saved'
-import Explore from './_root/pages/Explore'
-import AllUsers from './_root/pages/AllUsers'
-import CreatePost from './_root/pages/CreatePost'
-import EditPost from './_root/pages/EditPost'
-import PostDetails from './_root/pages/PostDetails'
-import Profile from './_root/pages/Profile'
-import UpdateProfile from './_root/pages/UpdateProfile'
+
 
 const App = () => {
     return (
