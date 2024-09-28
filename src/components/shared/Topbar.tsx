7b00c784bf1fb7afd8c9c100e8be23424011586b
Sina <sinaderik7@gmail.com>
2024-09-28 13:11:01 +0330

    feat: logo and signout button are added to the topbar

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import { Button } from "../ui/button"
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
+    <section className="topbar">
+      <div className="flex-between py-4 px-5">
+        <Link className="flex gap-3 items-center" to='/'>
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={130}
+            height={325}
+          />
+        </Link>
+        <div className="flex gap-4">
+          <Button
+            
+            className="shad-button_ghost"
+            variant='ghost'
+          >
+            <img src="/assets/icons/logout.svg" alt="logout" />
+          </Button>
+        </div>
+      </div>
+    </section>
   )
 }
 
