484db471c8ba66288d603411f01df4e3a24c0c91
Sina <sinaderik7@gmail.com>
2024-09-28 18:00:46 +0330

    feat: logic and styles are added to the  Bottombar component

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { bottombarLinks } from '@/constants'
+import { Link, useLocation } from 'react-router-dom'
 
 const Bottombar = () => {
+  const { pathname } = useLocation()
+
+
   return (
-    <div>Bottombar</div>
+    <section className='bottom-bar'>
+      {
+        bottombarLinks.map((link) => {
+          const isActive = link.route === pathname
+          return (
+
+            <Link
+              to={link.route}
+              key={link.label}
+              className={`${isActive && "bg-primary-500 rounded-[10px]"} flex-center flex-col gap-1 p-2 transition`}
+            >
+              <img
+                width={16}
+                height={16}
+                src={link.imgURL}
+                alt={link.label}
+                className={`${isActive && 'invert-white'}`}
+              />
+              <p className='tiny-medium text-light-2'>{link.label}</p>
+            </Link>
+
+          )
+        })
+      }
+    </section>
   )
 }
 
