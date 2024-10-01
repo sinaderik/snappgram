a8dca68427f7adda78d66148940bf90954b8daa2
Sina <sinaderik7@gmail.com>
2024-10-01 11:29:17 +0330

    feat: save button is added to the post

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -17,6 +17,17 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
                     onClick={() => { }}
                     className="cursor-pointer"
                 />
+                <p className="small-medium lg:base-medium">0</p>
+            </div>
+            <div className="flex gap-2 mr-5">
+                <img
+                    src="/assets/icons/save.svg"
+                    alt="like-icon"
+                    width={20}
+                    height={20}
+                    onClick={() => { }}
+                    className="cursor-pointer"
+                />
             </div>
         </div>
     )
