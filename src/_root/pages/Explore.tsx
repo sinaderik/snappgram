763d6ddbbca993abc5d0a15b210c72a301443737
Sina <sinaderik7@gmail.com>
2024-10-03 17:10:19 +0330

    feat: infinite scroll is implemented

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -3,18 +3,23 @@ import SearchResults from "@/components/shared/SearchResults"
 import useDebounce from "@/hooks/useDebounce"
 import { useGetPosts, useSearchPosts } from "@/lib/react-query/queriesAndMutations"
 import { Loader } from "lucide-react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useInView } from "react-intersection-observer"
 
 
 const Explore = () => {
 
+  const { ref,inView } = useInView();
   const { data: posts, fetchNextPage, hasNextPage } = useGetPosts();
 
   const [searchValue, setSearchValue] = useState("");
   const debouncedSearch = useDebounce(searchValue, 500);
   const { data: searchedPosts, isFetching: isSearchFetching } = useSearchPosts(debouncedSearch);
 
-  console.log(posts)
+  useEffect(()=>{
+    if(inView && !searchValue && hasNextPage) fetchNextPage()
+  },[inView,searchValue])
+
   if (!posts) {
     return <div className="flex-center w-full h-full">
       <Loader />
@@ -68,6 +73,11 @@ const Explore = () => {
             })
         }
       </div>
+      {hasNextPage && !searchValue &&(
+        <div ref={ref} className="mt-10">
+          <Loader />
+        </div>
+      )}
     </div>
   )
 }
